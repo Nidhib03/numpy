d2194fb525c89274afb551f3b2851167be03a6e9
NidhiB <100119660+Nidhib03@users.noreply.github.com>
2022-02-23 10:51:24 +0530

    Update numpy3.py

--- numpy3.py
+++ numpy3.py
@@ -1,15 +1,17 @@
 # indexing in numpy
 import numpy as np
 
-# An exemplar array
 arr = np.array([[-1, 2, 0, 4],
-				[4, -0.5, 6, 0],
-				[2.6, 0, 7, 8],
-				[3, -7, 4, 2.0]])
+		[4, -0.5, 6, 0],
+		[2.6, 0, 7, 8],
+		[3, -7, 4, 2.0]])
 print(arr.flatten())
+
 # Slicing array
-temp = arr[:4, :3]
-# temp = arr[::2, ::3]
+
+# temp = arr[:4, :3]           
+# temp = arr[::2, ::3]         # alternate rows and columns 
+temp = arr[:2, ::2]
 print ("Array with first 2 rows and alternate columns(0 and 2):\n", temp)
 
 # Integer array indexing example
